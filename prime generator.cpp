3a34e4c945cf26bf80c1cf2319162e185cea4d82
raghavrathi10 <raghav.rathi@hotmail.com>
2017-11-23 13:08:28 -0500

    please review

--- prime generator.cpp
+++ prime generator.cpp
@@ -1,29 +1,41 @@
 #include<iostream>
+#include<string>
 #include<cstdlib>
 #include<ctime>
 #include<math.h>
 using namespace std;
+void user_input();
 void fermat();
 int main()
 {
-	unsigned long int s_user1, s_user2,t_batemp,t_abtemp, t_ba,t_ab,shared_key, shared_keytemp;
-	cout << "User 1, please select your secret key = ";
+	
+	user_input();
+	//fermat();
+	return 0;
+}
+void user_input()
+{
+	unsigned long long int  t_batemp, t_abtemp, shared_key, shared_keytemp, t_ba, t_ab;
+	string n_user1, n_user2;
+	int s_user1, s_user2;
+	cout << "User 1 please enter your name: ";
+	cin >> n_user1;
+	cout << "User 2 please enter your name: ";
+	cin >> n_user2;
+	cout << endl << "Hi " << n_user1 << ", please select your secret key = " << endl;
 	cin >> s_user1;
-	cout << "User 2, please select your secret key = ";
+	cout << "Hi " << n_user2 << ", please select your secret key = " << endl;
 	cin >> s_user2;
 
 	t_batemp = pow(7, s_user1);
 	t_abtemp = pow(7, s_user2);
 	t_ab = t_abtemp % 11; //mod 11, because there was some problem in generating random prime so took 11 as temp. 
 	t_ba = t_batemp % 11;
-	cout << "The session key for user 1 is T_ba = " <<t_ba<<"\n";
+	cout << "The session key for user 1 is T_ba = " << t_ba << "\n";
 	cout << "The session key for user 2 is T_ab = " << t_ab << endl;
-	shared_keytemp = pow(pow(11, s_user1),s_user2);
+	shared_keytemp = pow(pow(11, s_user1), s_user2);
 	shared_key = shared_keytemp % 11;
-	cout << "The key to encrypt a message is : " << shared_key<<endl;
-
-	fermat();
-	return 0;
+	cout << "The key to encrypt a message is : " << shared_key << endl;
 }
 
 	void fermat()
